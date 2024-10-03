42927dc2c9dc4a9bc3571f8506a5d0f53c418693
Travis Barnette <barnette.travis31@gmail.com>
2024-10-03 09:39:49 -0400

    added coverage thresholds and test to meet that threshold

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,13 @@ export default {
     "<rootDir>/node_modules"
   ],
   coverageReporters:["json","html"],
-  testMatch:["<rootDir>/**/*.test.ts"]
+  testMatch:["<rootDir>/**/*.test.ts"],
+  coverageThreshold:{
+    "global":{
+      branches:90,
+      functions:90,
+      lines:90,
+      statements:90
+    }
+  }
 };
--- two-sum/index.test.ts
+++ two-sum/index.test.ts
@@ -4,5 +4,10 @@ describe('twoSum', ()=>{
     it('should find result of array that has two elements that add up to target', ()=>{
         const result = twoSum([1,3,0,-2,8,9], 10);
         expect(result).toEqual([0,5])
+    });
+    it('should return empty array when none meet the target sum', ()=>
+    {
+        const result = twoSum([0,0],1);
+        expect(result).toEqual([])
     })
 })
